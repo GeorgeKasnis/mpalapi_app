46ec3d5d4332b8104fecc8781b0c946cda118559
Giorgos Kalmoukis <giorgoskalmoukis@gmail.com>
2022-11-16 18:44:53 +0200

    Add championships

---
+++ pages/championships/index.vue
@@ -0,0 +1,92 @@
+<template>
+    <div>
+        <h1 class="text-2xl text-center mb-8 font-bold text-primary">Championships</h1>
+
+        <button @click="isOpen = true" class="text-primary mx-auto mb-8 bg-secondary w-12 h-12 rounded-full grid place-items-center text-4xl shadow-md shadow-gray-500">+</button>
+
+        <transition name="fade-in-out">
+            <div class="grid grid-cols-2 gap-8 max-w-2xl mx-auto" v-if="!loading">
+                <UIBaseCard v-for="(championship, index) in championships" :key="index" :name="championship.title" :link="`/championships/${championship.id}`" @delete="deleteChampionship(championship.id)" deletable="true" />
+            </div>
+        </transition>
+
+        <transition name="fade-in-out">
+            <UIBaseModal v-if="isOpen" @close-modal="isOpen = false">
+                <h2 class="text-xl mx-auto mb-2 text-center">Add Team</h2>
+                <form @submit.prevent="addChampionship" class="[&>*]:mb-4">
+                    <div><input v-model="championshipTitle" class="border text-white" placeholder="Title" type="text" /></div>
+                    <ButtonsBaseBtn name="Add Championship" />
+                </form>
+            </UIBaseModal>
+        </transition>
+
+        <UIBaseLoadingSpinner v-if="loading" />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            championships: {},
+            championshipTitle: "",
+            isOpen: false,
+            loading: true,
+            baseUrl: null,
+        };
+    },
+    mounted() {
+        const config = useRuntimeConfig();
+        this.baseUrl = config.BASE_URL;
+        this.getChampionships();
+    },
+    methods: {
+        async getChampionships() {
+            await $fetch(`${this.baseUrl}/api/championships`, {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((response) => {
+                    this.championships = response;
+                    this.loading = false;
+                });
+                
+        },
+        async addChampionship() {
+            await $fetch(`${this.baseUrl}/api/championships`, {
+                    method: "POST",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ title: this.championshipTitle }),
+                })
+                .then((response) => {
+                   this.getChampionships()
+                   this.isOpen = false;
+                   this.championshipTitle = "";
+                });
+
+            
+        },
+        async deleteChampionship(id) {
+            await $fetch(`${this.baseUrl}/api/championships/${id}`, {
+                method: "DELETE",
+                headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                    },
+            }).then(() => {
+                this.getChampionships();
+                this.isOpen = false;
+                this.championshipTitle = "";
+            });
+
+            
+        },
+    },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
         <div class="grid mx-auto aspect-square grid-cols-2 gap-8 h-[calc(100vh-10rem)]">
             <nuxt-link class="bg-primary shadow-lg ball rounded-full shadow-gray-500 text-white grid place-items-center text-5xl" to="/teams"><span class="relative z-10">Teams</span></nuxt-link>
             <nuxt-link class="bg-primary shadow-lg ball rounded-full shadow-gray-500 text-white grid place-items-center text-5xl" to="/players"><span class="relative z-10">Players</span></nuxt-link>
-            <nuxt-link class="bg-primary shadow-lg ball rounded-full shadow-gray-500 text-white grid place-items-center text-5xl" to="/statistics"><span class="relative z-10">Statistics</span></nuxt-link>
-            <nuxt-link class="bg-primary shadow-lg ball rounded-full shadow-gray-500 text-white grid place-items-center text-5xl" to="/scores"><span class="relative z-10">Scores</span></nuxt-link>
+            <nuxt-link class="bg-primary shadow-lg ball rounded-full shadow-gray-500 text-white grid place-items-center text-5xl" to="/championships"><span class="relative z-10">Championships</span></nuxt-link>
+            <nuxt-link class="bg-primary shadow-lg ball rounded-full shadow-gray-500 text-white grid place-items-center text-5xl" to="/"><span class="relative z-10">Scores</span></nuxt-link>
         </div>
     </div>
 </template>
