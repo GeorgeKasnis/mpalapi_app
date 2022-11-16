a79043a6cc67cbd98a42ac6cf41ce6ddf1cf9f0a
Giorgos Kalmoukis <giorgoskalmoukis@gmail.com>
2022-11-16 18:17:13 +0200

    Finish attach detach players

--- components/UI/BaseCard.vue
+++ components/UI/BaseCard.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="text-xl text-primary bg-secondary rounded-lg shadow-md shadow-gray-500 relative z-10">
         <nuxt-link :to="link" class="relative after:grid after:place-items-center after:text-3xl after:content-['👁'] after:absolute after:inset-0 after:rounded-lg after:pointer-events-none after:bg-black after:opacity-0 hover:after:opacity-80 after:transition-all">
-            <img v-if="imgSrc" :src="imgSrc" class="w-full aspect-square object-cover rounded-t-lg" alt="" />
             <div class="text-center truncate transition px-12 py-2 block">{{ name }}</div>
         </nuxt-link>
         <button v-if="deletable" @click="$emit('delete')" class="absolute  text-base right-2 top-2 w-6 h-6 rotate-45 text-red-800 bg-white/10 hover:bg-white/80 rounded-full grid place-items-center">+</button>
@@ -11,6 +10,6 @@
 
 <script>
 export default {
-    props: ["link", "imgSrc", "name","deletable","number"],
+    props: ["link", "name","deletable","number"],
 };
 </script>
--- pages/teams/[id].vue
+++ pages/teams/[id].vue
@@ -1,20 +1,15 @@
 <template>
     <div>
         <h1 class="text-2xl text-center mb-8 font-bold text-primary">{{ team.name }}</h1>
-
+        
         <h2 class="text-2xl text-center mb-8 font-bold text-primary">Team Players</h2>
 
-        <button @click="isOpen = true" class="text-primary mx-auto mb-8 bg-secondary w-12 h-12 rounded-full grid place-items-center text-4xl shadow-md shadow-gray-500">+</button>
+        <button v-if="team.players" @click="isOpen = true" class="text-primary mx-auto mb-8 bg-secondary w-12 h-12 rounded-full grid place-items-center text-4xl shadow-md shadow-gray-500">+</button>
 
         <div class="grid grid-cols-2 gap-8 max-w-2xl mx-auto">
-            <!-- <div noPadding="true" class="text-xl text-primary bg-secondary relative py-8" v-for="(player, index) in team.players" :key="index">
-                <nuxt-link to="#" class="relative after:grid after:place-items-center after:text-3xl after:content-['👁'] after:absolute after:inset-0  after:pointer-events-none after:bg-black after:opacity-0 hover:after:opacity-40 after:transition-all">
-                    <div class="text-center px-4 transition rounded-t-full py-2 block ">{{ player.name }}</div>
-                    <div class="text-center px-4 transition rounded-t-full py-2 block ">{{ player.number }}</div>
-                </nuxt-link>
-                <button class="absolute right-2 top-2 w-8 h-8 rotate-45 text-red-800 bg-white/10 rounded-full grid place-items-center">+</button>
-            </div> -->
-            <UIBaseCard img-src ="https://media.fast4foos.org/photos/players/84000895.jpg" number="7" link="/players/player1" deletable="true" @delete="detatchPlayer" name="George" />
+            <div v-for="(player, index) in team.players" :key="index" >
+                <UIBaseCard :number="player.number" deletable="true" @delete="detatchPlayer(player.id)" :name="player.name" />
+            </div >
         </div>
 
         <transition name="fade-in-out">
@@ -22,8 +17,8 @@
                 <h2 class="text-xl mx-auto mb-2 text-center">Add Player to this team</h2>
                 <form @submit.prevent="attachPlayer" class="[&>*]:mb-4">
                     <div>
-                        <select v-model="player.name" class="border w-full text-black">
-                            <option value="{{player.id}}" v-for="(player, index) in allPlayers" :key="index">{{ player.name }}</option>
+                        <select v-model="playerId" class="border w-full text-black">
+                            <option :value="player.id" v-for="(player, index) in allPlayers" :key="index">{{ player.name }}</option>
                         </select>
                     </div>
                     <ButtonsBaseBtn name="Add Player" />
@@ -40,9 +35,7 @@ export default {
             team: {},
             allPlayers: {},
             isOpen: false,
-            player: {
-                name: "",
-            },
+            playerId : null
         };
     },
     mounted() {
@@ -68,25 +61,24 @@ export default {
                     "Content-Type": "application/json",
                 },
                 body: {
-                    player_id: 1,
+                    player_id: this.playerId,
                 },
             })
-                .then((response) => {
-                    //notify
-                })
-                .then(() => {
-                    this.isOpen = false;
-                })
-                .then((response) => (this.team = response));
+            .then((response) => {
+                console.log(response);
+                this.playerId = null;
+                this.isOpen = false;
+                this.getTeam(this.team.id)
+            });
         },
-        async detatchPlayer() {
-            $fetch(`http://127.0.0.1:8000/api/teams/${this.team.id}/players`, {
+        async detatchPlayer(id) {
+            $fetch(`http://127.0.0.1:8000/api/teams/${this.team.id}/players/${id}`, {
                 method: "DELETE",
                 headers: {
                     Accept: "application/json",
                     "Content-Type": "application/json",
                 },
-            }).then(() => (this.team = this.getTeam(this.team.id)));
+            }).then(() => (this.getTeam(this.team.id)));
         },
         async getAllPlayers() {
             $fetch(`http://127.0.0.1:8000/api/players/`, {
