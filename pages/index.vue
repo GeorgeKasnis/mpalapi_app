3b6d5e5ce02aaf59fdadb1a0a9aed3f8c0a73093
Giorgos Kalmoukis <giorgoskalmoukis@gmail.com>
2022-11-17 13:11:26 +0200

    Add results page

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
             <nuxt-link class="bg-primary aspect-square shadow-lg ball rounded-full shadow-gray-500 text-white grid place-items-center text-4xl" to="/players"><span class="relative z-10">Players</span></nuxt-link>
             <nuxt-link class="bg-primary aspect-square shadow-lg ball rounded-full shadow-gray-500 text-white grid place-items-center text-4xl" to="/championships"><span class="relative z-10">Championships</span></nuxt-link>
             <nuxt-link class="bg-primary aspect-square shadow-lg ball rounded-full shadow-gray-500 text-white grid place-items-center text-4xl" to="/games"><span class="relative z-10">Games</span></nuxt-link>
+            <nuxt-link class="bg-primary aspect-square shadow-lg ball rounded-full shadow-gray-500 text-white grid place-items-center text-4xl" to="/results"><span class="relative z-10">Results</span></nuxt-link>
         </div>
     </div>
 </template>
---
+++ pages/results/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        {{results}}
+        <h1 class="text-2xl text-center mb-8 font-bold text-primary">Results</h1>
+
+        <transition name="fade-in-out">
+            <div class="grid grid-cols-1 gap-8 max-w-2xl mx-auto" v-if="!loading">
+                <UIBaseCard v-for="(team, index) in results" :key="index" :name="team.name" :number="team.points" :link="`/teams/${team.id}`"/>
+            </div>
+        </transition>
+
+        <UIBaseLoadingSpinner v-if="loading" />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            results: {},
+            loading: true,
+            baseUrl: null,
+        };
+    },
+    mounted() {
+        const config = useRuntimeConfig();
+        this.baseUrl = config.BASE_URL;
+        this.getResults();
+    },
+    methods: {
+        async getResults() {
+            await $fetch(`${this.baseUrl}/api/results`, {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((response) => {
+                    this.results = response;
+                    this.loading = false;
+                });
+                
+        }
+    },
+};
+</script>
